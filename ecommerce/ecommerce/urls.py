949d8d29946cad1a939cc2fce0567b0fb76bd7b3
Ahmad-Mukhtar <66535485+Ahmad-Mukhtar@users.noreply.github.com>
2021-12-15 03:59:39 +0500

    Minor changes

--- ecommerce/ecommerce/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.homepage, name='HomePage'),
-    path('login', views.login, name='login'),
-    path('register', views.register, name='register'),
-]
