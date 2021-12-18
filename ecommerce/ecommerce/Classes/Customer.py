b30acedc30b7f989ca505bf379b61f1d6e69b005
Ahmad-Mukhtar <66535485+Ahmad-Mukhtar@users.noreply.github.com>
2021-12-18 17:07:38 +0500

    Delete Account Done

---
+++ ecommerce/ecommerce/Classes/Customer.py
@@ -0,0 +1,9 @@
+
+class customer:
+    def __init__(self,cust_id,cust_name,cust_password,cust_email):
+        self.ID=cust_id
+        self.name=cust_name
+        self.password=cust_password
+        self.email=cust_email
+
+
